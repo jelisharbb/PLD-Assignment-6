89de9cc4282f35145ac15dd7fea36322eac8c683
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-20 20:12:25 +0800

    Defined variables that would ask numbers from the user.

---
+++ Prog-1-Number-Sorter.py
@@ -0,0 +1,12 @@
+# Program 1: Number Sorter
+# Create a program that ask 4 numbers. 
+# Print the 4 numbers from highest to lowest using only if-else statement.
+
+# printed a greetings
+print ("Welcome! This program can arrange four (4) numbers from highest to lowest.")
+
+# ask inputs from the user
+firstNum = float(input("Enter your first number: "))
+secondNum = float(input("Enter your second number: "))
+thirdNum = float(input("Enter your third number: "))
+fourthNum = float(input("Enter your fourth number: "))
