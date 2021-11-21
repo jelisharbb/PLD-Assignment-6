d391e12ab14d9945866326dfd9489a75a4a42827
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-21 14:52:12 +0800

    Added six if elif conditions given that the first number is the highest number.

--- Prog-1-Number-Sorter.py
+++ Prog-1-Number-Sorter.py
@@ -3,10 +3,26 @@
 # Print the 4 numbers from highest to lowest using only if-else statement.
 
 # printed a greetings
-print ("Welcome! This program can arrange four (4) numbers from highest to lowest.")
+print ("\nWelcome! This program can arrange four (4) numbers from highest to lowest.")
 
 # ask inputs from the user
-firstNum = float(input("Enter your first number: "))
+firstNum = float(input("\nEnter your first number: "))
 secondNum = float(input("Enter your second number: "))
 thirdNum = float(input("Enter your third number: "))
 fourthNum = float(input("Enter your fourth number: "))
+
+# if elif else conditions of 24 possible results
+
+# six (6) possible results if the first number is the highest number
+if firstNum > secondNum > thirdNum > fourthNum:
+    print (f"\nDescending order: {firstNum}, {secondNum}, {thirdNum}, {fourthNum} \n")
+elif firstNum > thirdNum > fourthNum > secondNum:
+    print (f"\nDescending order: {firstNum}, {thirdNum}, {fourthNum}, {secondNum} \n")
+elif firstNum > fourthNum > secondNum > thirdNum:
+    print (f"\nDescending order: {firstNum}, {fourthNum}, {secondNum}, {thirdNum} \n")
+elif firstNum > secondNum > fourthNum > thirdNum:
+    print (f"\nDescending order: {firstNum}, {secondNum}, {fourthNum}, {thirdNum} \n")
+elif firstNum > thirdNum > secondNum > fourthNum:
+    print (f"\nDescending order: {firstNum}, {thirdNum}, {secondNum}, {fourthNum} \n")
+elif firstNum > fourthNum > thirdNum > secondNum:
+    print (f"\nDescending order: {firstNum}, {fourthNum}, {thirdNum}, {secondNum} \n")
