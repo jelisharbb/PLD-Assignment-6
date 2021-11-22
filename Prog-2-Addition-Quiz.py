a2f3b4ab58b66ca3c502a9dbdef7ad3c202de606
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-22 19:50:01 +0800

    Printed greetings to the user.

---
+++ Prog-2-Addition-Quiz.py
@@ -0,0 +1,12 @@
+# Program 2: Addition Quiz
+# Create a program that automatically generate two random numbers to add (0 to 99)
+# Let the user answer and evaluate if the user has the correct answer
+# The program will ask the user 10 addition operation
+# Display the result summary of the 10 operations (ex 9/10)
+
+# greetings to the user
+print ("\nWelcome to Jelisha's Addition Quiz! \nThis is a ten (10) item quiz that will test your mathematical skills in adding random numbers from 0 to 99.")
+
+userName = input("\nPlease enter your name:\n")
+
+ready = input(f"\nHey, {userName}. Are you ready to take the quiz?\n")
